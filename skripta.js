5500fadadcbdc9861743ac5e5639bc4a9dd9d29a
KidX22 <avdickaid123@gmail.com>
2021-12-07 19:15:47 +0100

    uradio dosta sranja

--- skripta.js
+++ skripta.js
@@ -1,9 +1,16 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks=document.getElementsByClassName('navbar-links')[0]
+const parallax= document.getElementById("div1")
 
 
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+// parallax pozadina
 
-    toggleButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active')
-    })
+window.addEventListener('scroll', function(){
+    let offset = window.scrollY;
+    parallax.style.backgroundPositionY = offset*0.7+"px";
+
+})
 
