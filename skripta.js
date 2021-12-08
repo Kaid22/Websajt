637f5e6c52cdda51ec835be6a1a923dda8c774bf
KidX22 <avdickaid123@gmail.com>
2021-12-09 00:32:10 +0100

    ruargh komita

--- skripta.js
+++ skripta.js
@@ -1,16 +1,59 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks=document.getElementsByClassName('navbar-links')[0]
-const parallax= document.getElementById("div1")
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const parallax = document.getElementById("div1");
 
+toggleButton.addEventListener("click", () => {
+	navbarLinks.classList.toggle("active");
+});
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
 // parallax pozadina
 
-window.addEventListener('scroll', function(){
-    let offset = window.scrollY;
-    parallax.style.backgroundPositionY = offset*0.7+"px";
+window.addEventListener("scroll", function () {
+	let offset = window.scrollY;
+	parallax.style.backgroundPositionY = offset * 0.8 + "px";
+});
 
-})
+//slideshow
 
+const myslide = document.querySelectorAll(".myslide"),
+	dot = document.querySelectorAll(".dot");
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+	let i;
+	for (i = 0; i < myslide.length; i++) {
+		myslide[i].style.display = "none";
+	}
+	for (i = 0; i < dot.length; i++) {
+		dot[i].className = dot[i].className.replace(" active", "");
+	}
+	if (n > myslide.length) {
+		counter = 1;
+	}
+	if (n < 1) {
+		counter = myslide.length;
+	}
+	myslide[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+}
